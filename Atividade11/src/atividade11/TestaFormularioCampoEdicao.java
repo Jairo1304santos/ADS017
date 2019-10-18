572c22597486ad16bb9b2688eaf3735c4fb08b54
jairo <jairo@10.30.33.17>
2019-10-17 22:28:40 -0300

    atividade 11 concluida

---
+++ Atividade11/src/atividade11/TestaFormularioCampoEdicao.java
@@ -0,0 +1,13 @@
+
+package atividade11;
+
+
+public class TestaFormularioCampoEdicao {
+    public static void main(String[] args) {
+        
+        FormularioCampoEdicao formulario = new FormularioCampoEdicao();
+        
+        formulario.setVisible(true);
+    }
+    
+}
